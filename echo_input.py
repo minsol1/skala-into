99d32bd15f3bb7bfeef581714c7f366f086cd184
김민솔 <artkms4815@naver.com>
2026-07-14 16:45:01 +0900

    feat : echo(1/2)

---
+++ echo_input.py
@@ -0,0 +1,15 @@
+#
+# 교육 환경 설정 및 간단한 파이썬 연습 코드
+# [실습1] Echo 프로그램 (1/2)
+# 사용자가 입력하는 문장을 그대로 출력하는 프로그램
+#
+# 작성일 : 2026-07-14
+# 작성자 : 김민솔, SKALA
+#
+# 변경일 : 
+#
+# All Rights Reserved by SK AX, SKALA
+#
+
+sentence = input("문장을 입력하세요: ")
+print(sentence)
